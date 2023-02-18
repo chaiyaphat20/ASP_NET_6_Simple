3f843fd875d18e824528f54f628c4cf713f0a3d6
chaiyaphat <chaiyaphat.b5922314@gmail.com>
2023-02-18 21:58:47 +0700

    [FEAT]:Init first connect db

--- ASPNET7LIVE/Program.cs
+++ ASPNET7LIVE/Program.cs
@@ -4,13 +4,13 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
+//>>Add database โดย APIContect คือตัวแทน DB
 builder.Services.AddDbContext<APIContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("APIContext")));
+//--//
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -19,6 +19,8 @@ builder.Services.AddSwaggerGen();
 //AddScoped -> ทุกๆ req จะ new create intance พอเสร็จก็ตายไป
 //ทำให้เรียกใช้ Class Thaidate ได้  
 builder.Services.AddScoped<IThaiDate, ThaiDate>();
+//--//
+
 
 var app = builder.Build();
 
