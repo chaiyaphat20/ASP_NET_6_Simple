93de568ce20ead071da0711a298debffeef8cfe1
chaiyaphat <chaiyaphat.b5922314@gmail.com>
2023-02-19 16:29:00 +0700

    [FEAT]: RAW SQL

--- ASPNET7LIVE/Controllers/CategoryController.cs
+++ ASPNET7LIVE/Controllers/CategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using ASPNET7LIVE.Data;
-using Microsoft.AspNetCore.Authorization;
+using ASPNET7LIVE.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ASPNET7LIVE.Controllers
 {
@@ -13,12 +14,29 @@ namespace ASPNET7LIVE.Controllers
         {
             _context = context;
         }
-        // GET: api/Category/5
+
+        // GET ALL : api/Category
+        // Task<ActionResult<IEnumerable<Category>>>
+        [HttpGet]
+        public async Task<IActionResult> GetCategory()
+        {
+            //AsNoTracking() ใช้เพื่อทำให้เร็วขึ้น กรณีที่เรา ไม่ได้เอา ตัวแปรไปใช้งาต่อ
+            //ถ้าเรา เอาตัวแปร catgory ไปใช้งานต่อ ห้ามใช้
+            //OrderByDescending(c=>c.CategoryId) คือต้องการเรียงโดยใช้ CategoryId จากมากไปน้อย
+            var category = await _context.Category.OrderByDescending(c=>c.CategoryId).AsNoTracking().ToListAsync();
+            var categoryFromSQL = await _context.Category.FromSqlRaw("select * from Category order by CategoryId desc").ToListAsync();
+
+            var total =await _context.Category.CountAsync();
+            return  Ok(new {toTALrOW = total,data = categoryFromSQL });
+        }
+
+
+        // GET BY ID: api/Category/5
         [HttpGet("{id}")]
         public async Task<IActionResult> GetCategory(int id)
         {
             var category = await _context.Category.FindAsync(id);
-
+           
             if (category == null)
             {
                 return NotFound(new { message = "ไม่พบข้อมูลนี้ในระบบ" });
