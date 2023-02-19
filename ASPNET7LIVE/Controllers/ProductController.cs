77e56ed12ab9b3427209f881b66bad9390546687
chaiyaphat <chaiyaphat.b5922314@gmail.com>
2023-02-19 22:10:53 +0700

    add product controller

---
+++ ASPNET7LIVE/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using ASPNET7LIVE.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPNET7LIVE.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProductController : Controller
+    {
+        private readonly APIContext _context;
+
+        public ProductController(APIContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Getproduct()
+        {
+
+            var product = await _context.Productc.OrderByDescending(c => c.ProductId).AsNoTracking().ToListAsync();
+            var total = await _context.Productc.CountAsync();
+            return Ok(new { toTALrOW = total, data = product });
+        }
+    }
+}
